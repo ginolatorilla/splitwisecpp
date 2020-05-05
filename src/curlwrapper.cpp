2ec861c97dfc77d66d079c7f6b7f4a79f5f1edb8
Gino Latorilla <pblatorilla+github@gmail.com>
2020-05-05 09:34:33 +0800

    Parse JSON directly, no more temporary stream

--- src/curlwrapper.cpp
+++ src/curlwrapper.cpp
@@ -13,6 +13,15 @@ namespace detail
         reinterpret_cast<std::ostream*>(stream)->write(data, size * nmemb);
         return size * nmemb; 
     }
+
+    int write_to_json_reader(char* data, size_t size, size_t nmemb, void* context)
+    {
+        assert(context != nullptr);
+        auto* reader = reinterpret_cast<JsonReaderContext*>(context)->reader;
+        auto* json = reinterpret_cast<JsonReaderContext*>(context)->json;
+        reader->parse(data, data + size * nmemb, json, nullptr);
+        return size * nmemb; 
+    }
 }
 
 Curl::Curl() : handle(curl_easy_init())
@@ -29,6 +38,12 @@ void Curl::set_write_to_stream(std::ostream* stream)
 {
     ::curl_easy_setopt(handle, CURLOPT_WRITEFUNCTION, detail::write_to_ostream);
     ::curl_easy_setopt(handle, CURLOPT_WRITEDATA, stream);
+}
+
+void Curl::set_write_to_json(detail::JsonReaderContext* context)
+{
+    ::curl_easy_setopt(handle, CURLOPT_WRITEFUNCTION, detail::write_to_json_reader);
+    ::curl_easy_setopt(handle, CURLOPT_WRITEDATA, context);
 }
 
 void Curl::set_url(const char* url)
--- src/curlwrapper.hpp
+++ src/curlwrapper.hpp
@@ -1,8 +1,18 @@
 #include <curl/curl.h>
 #include <iosfwd>
+#include <json/reader.h>
 
 namespace splitwisecpp
 {
+
+namespace detail
+{
+    struct JsonReaderContext
+    {
+        ::Json::CharReader* reader;
+        ::Json::Value* json;
+    };
+}
 
 class Curl
 {
@@ -11,6 +21,7 @@ public:
     ~Curl();
 
     void set_write_to_stream(std::ostream* stream);
+    void set_write_to_json(detail::JsonReaderContext* stream);
     void set_url(const char* url);
     void perform();
 
