be707ed27373aaf046ec7cf8ba9b3c67dff89e75
Gino Latorilla <pblatorilla+github@gmail.com>
2020-05-05 09:16:10 +0800

    Extract API traits

--- src/splitwisecpp.cpp
+++ src/splitwisecpp.cpp
@@ -2,6 +2,7 @@
 
 #include "splitwisecpp/splitwisecpp.h"
 #include "curlwrapper.hpp"
+#include "api_traits.hpp"
 #include <oauth.h>
 #include <array>
 #include <algorithm>
@@ -14,23 +15,17 @@ namespace splitwisecpp
 
 static const std::string BASEURL = "https://www.splitwise.com/api/v3.0/";
 
-enum class Methods : uint8_t
-{
-    get_current_user
-};
-
 struct Context
 {
     Curl curl;
-
     std::array<char*, 1> signed_urls;
 };
 
 Splitwise::Splitwise(const Configuration& config)
 {
     _context = new Context();
-    AS_CONTEXT(_context)->signed_urls[static_cast<uint8_t>(Methods::get_current_user)] = 
-        ::oauth_sign_url2((BASEURL + "get_current_user").c_str(),
+    AS_CONTEXT(_context)->signed_urls[static_cast<uint8_t>(api_traits<ApiMethods::get_current_user>::id)] = 
+        ::oauth_sign_url2((BASEURL + api_traits<ApiMethods::get_current_user>::c_str).c_str(),
                           nullptr, // unused
                           ::OA_HMAC,
                           nullptr, // HTTP GET
@@ -50,12 +45,13 @@ Splitwise::~Splitwise()
     delete AS_CONTEXT(_context);
 }
 
+
 Json Splitwise::get_current_user()
 {
     std::stringstream response_sink;
     AS_CONTEXT(_context)->curl.set_write_to_stream(&response_sink);
     AS_CONTEXT(_context)->curl.set_url(
-        AS_CONTEXT(_context)->signed_urls[static_cast<uint8_t>(Methods::get_current_user)]
+        AS_CONTEXT(_context)->signed_urls[static_cast<uint8_t>(api_traits<ApiMethods::get_current_user>::id)]
     );
     AS_CONTEXT(_context)->curl.perform();
 
