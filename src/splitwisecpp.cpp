d4f097891dfcf07b9067a90e62412036d58607da
Gino Latorilla <pblatorilla+github@gmail.com>
2020-05-05 10:00:03 +0800

    Extract signed URL assembler to Context

--- src/splitwisecpp.cpp
+++ src/splitwisecpp.cpp
@@ -20,6 +20,21 @@ struct Context
 {
     Curl curl;
     std::array<char*, 1> signed_urls;
+    const Configuration* config;
+
+    Context(const Configuration* config_) : config(config_)
+    {
+        assemble_signed_url<ApiMethods::get_current_user>();
+    }
+
+    ~Context()
+    {
+        for (auto*& url : signed_urls)
+        {
+            ::free(url);
+            url = nullptr;
+        }
+    }
 
     template<ApiMethods M>
     Json api_request_as_json()
@@ -37,29 +52,30 @@ struct Context
 
         return parsed;
     }
+
+    template<ApiMethods M>
+    void assemble_signed_url()
+    {
+        signed_urls[api_traits<M>::id] = ::oauth_sign_url2(
+            (BASEURL + api_traits<M>::c_str).c_str(),
+            nullptr, // unused
+            ::OA_HMAC,
+            nullptr, // HTTP GET
+            config->consumer_key.c_str(),
+            config->consumer_secret.c_str(),
+            config->oauth1_token.c_str(),
+            config->oauth1_token_secret.c_str()
+        );
+    }
 };
 
 Splitwise::Splitwise(const Configuration& config)
 {
-    _context = new Context();
-    AS_CONTEXT(_context)->signed_urls[static_cast<uint8_t>(api_traits<ApiMethods::get_current_user>::id)] = 
-        ::oauth_sign_url2((BASEURL + api_traits<ApiMethods::get_current_user>::c_str).c_str(),
-                          nullptr, // unused
-                          ::OA_HMAC,
-                          nullptr, // HTTP GET
-                          config.consumer_key.c_str(),
-                          config.consumer_secret.c_str(),
-                          config.oauth1_token.c_str(),
-                          config.oauth1_token_secret.c_str());
+    _context = new Context(&config);
 }
 
 Splitwise::~Splitwise()
 {
-    for (auto*& url : AS_CONTEXT(_context)->signed_urls)
-    {
-        ::free(url);
-        url = nullptr;
-    }
     delete AS_CONTEXT(_context);
 }
 
