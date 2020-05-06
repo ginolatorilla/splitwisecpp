1c8699bab60e1345db55f67cd12f6cf18ab1d0f5
Gino Latorilla <pblatorilla+github@gmail.com>
2020-05-06 16:46:41 +0800

    Extract types that will be exported

--- include/splitwisecpp/splitwisecpp.h
+++ include/splitwisecpp/splitwisecpp.h
@@ -3,41 +3,14 @@
 #ifndef SPLITWISECPP_H_
 #define SPLITWISECPP_H_
 
-#include <string>
-#include <json/value.h>
+#include "types.h"
 
 namespace splitwisecpp
 {
 
-using UnspecifiedPtr = void*;
-using Json = ::Json::Value;
-using UserId = std::uint32_t;
-
-struct Configuration
-{
-    std::string consumer_key;
-    std::string consumer_secret;
-    std::string oauth1_token;
-    std::string oauth1_token_secret;
-};
-
-enum class ErrorCodes
-{
-    NoError,
-    OAuth1_Sign_Url_Error,
-    OutOfMemory,
-    NetworkError
-};
-
 class Splitwise
 {
 public:
-    struct ApiResponse
-    {
-        Json json;
-        ErrorCodes error;
-    };
-
     explicit Splitwise(const Configuration& config);
     ~Splitwise();
 
