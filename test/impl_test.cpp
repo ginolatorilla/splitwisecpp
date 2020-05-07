7ab90197789d06c401007e101877953984c68f02
Gino Latorilla <pblatorilla+github@gmail.com>
2020-05-07 21:07:18 +0800

    Added generic use cases tests

--- test/impl_test.cpp
+++ test/impl_test.cpp
@@ -10,7 +10,7 @@
 
 namespace testing
 {
-TEST_F(splitwisecpp_api_tests, get_current_user)
+TEST_F(splitwisecpp_api_tests, generic_respond_with_json_string)
 {
     expect_for_http_get_api_request("get_current_user");
     EXPECT_CALL(mock_curl(), curl_easy_perform(dummy_curl))
@@ -21,6 +21,35 @@ TEST_F(splitwisecpp_api_tests, get_current_user)
 
     auto user = splitwise->get_current_user();
     ASSERT_STREQ(user.json["user"]["last_name"].asCString(), "yeet");
+    ASSERT_EQ(splitwisecpp::ErrorCodes::NoError, user.error);
 }
 
+TEST_F(splitwisecpp_api_tests, generic_cannot_find_server)
+{
+    expect_for_http_get_api_request("get_current_user");
+    EXPECT_CALL(mock_curl(), curl_easy_perform(dummy_curl))
+        .Times(1)
+        .WillOnce(Return(CURLE_COULDNT_RESOLVE_HOST));
+
+    auto user = splitwise->get_current_user();
+    ASSERT_EQ(splitwisecpp::ErrorCodes::NetworkError, user.error);
+}
+
+TEST_F(splitwisecpp_api_tests, DISABLED_generic_out_of_memory)
+{
+    EXPECT_CALL(mock_curl(), curl_easy_setopt_voidp(dummy_curl, CURLOPT_URL, _))
+        .Times(1)
+        .WillOnce(Return(CURLE_OUT_OF_MEMORY));
+
+    auto user = splitwise->get_current_user();
+    ASSERT_EQ(splitwisecpp::ErrorCodes::OutOfMemory, user.error);
+}
+
+TEST_F(splitwisecpp_api_tests, get_current_user)
+{
+    expect_for_http_get_api_request("get_current_user");
+    EXPECT_CALL(mock_curl(), curl_easy_perform(dummy_curl)).Times(1);
+    auto user = splitwise->get_current_user();
+    ASSERT_EQ(splitwisecpp::ErrorCodes::NoError, user.error);
+}
 }  // namespace testing
