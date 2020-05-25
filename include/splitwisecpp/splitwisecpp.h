4646da932acdb20a802175519618b9e41f885513
Gino Latorilla <pblatorilla+github@gmail.com>
2020-05-25 19:21:00 +0800

    Generate header file from descriptors with Jinja2

--- include/splitwisecpp/splitwisecpp.h
+++ include/splitwisecpp/splitwisecpp.h
@@ -17,7 +17,6 @@ public:
     ApiResponse get_current_user();
     ApiResponse get_user(IdType id);
     ApiResponse get_groups();
-
     ApiResponse create_group(const Json& group);
     ApiResponse delete_group(IdType id);
 
@@ -28,3 +27,6 @@ private:
 }  // namespace splitwisecpp
 
 #endif  // SPLITWISECPP_H_
+
+// DO NOT EDIT BY HAND!
+// 'tools/generate_cpp_api_from_descriptor' created me on 2020-05-25T22:18:47.190784 from descriptor version 0.9
