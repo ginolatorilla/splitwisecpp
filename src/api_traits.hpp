cc1234bfb37557cf18c5dc26c429586a97248d13
Gino Latorilla <pblatorilla+github@gmail.com>
2020-05-25 22:38:12 +0800

    Generate API type traits from method descriptors

--- src/api_traits.hpp
+++ src/api_traits.hpp
@@ -14,7 +14,7 @@ enum class ApiMethods : std::uint32_t
     get_user,
     get_groups,
     create_group,
-    delete_group,
+    delete_group
 };
 
 enum class HttpMethods
@@ -27,21 +27,39 @@ template<ApiMethods M>
 struct api_traits
 {};
 
-#define API_TRAITS_ENTRY(NAME, HTTPMETHOD) \
-    template<> struct api_traits<ApiMethods::NAME> \
-    { \
-        static constexpr const char* const c_str = #NAME; \
-        static constexpr HttpMethods http_method = HTTPMETHOD; \
-    }
+template<> struct api_traits<ApiMethods::get_current_user>
+{
+    static constexpr const char* const c_str = "get_current_user";
+    static constexpr HttpMethods http_method = HttpMethods::Get;
+};
 
-API_TRAITS_ENTRY(get_current_user, HttpMethods::Get);
-API_TRAITS_ENTRY(get_user, HttpMethods::Get);
-API_TRAITS_ENTRY(get_groups, HttpMethods::Get);
-API_TRAITS_ENTRY(create_group, HttpMethods::Post);
-API_TRAITS_ENTRY(delete_group, HttpMethods::Post);
+template<> struct api_traits<ApiMethods::get_user>
+{
+    static constexpr const char* const c_str = "get_user";
+    static constexpr HttpMethods http_method = HttpMethods::Get;
+};
 
-#undef API_TRAITS_ENTRY
+template<> struct api_traits<ApiMethods::get_groups>
+{
+    static constexpr const char* const c_str = "get_groups";
+    static constexpr HttpMethods http_method = HttpMethods::Get;
+};
 
-}  // splitwisecpp
+template<> struct api_traits<ApiMethods::create_group>
+{
+    static constexpr const char* const c_str = "create_group";
+    static constexpr HttpMethods http_method = HttpMethods::Post;
+};
+
+template<> struct api_traits<ApiMethods::delete_group>
+{
+    static constexpr const char* const c_str = "delete_group";
+    static constexpr HttpMethods http_method = HttpMethods::Post;
+};
+
+}  // namespace splitwisecpp
 
 #endif  // SPLITWISECPP_API_TRAITS_H_
+
+// DO NOT EDIT BY HAND!
+// 'tools/generate_cpp_api_from_descriptor' created me on 2020-05-25T22:36:40.078685 from descriptor version 0.9
